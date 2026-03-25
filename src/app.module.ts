42918621c67a8969c98ebd785208fa3843b6fa6e
Vincent <vincentkiathadi@gmail.com>
2026-03-26 03:37:57 +0800

    change to supabase

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,14 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule,
+        ConfigModule.forRoot({
+            isGlobal: true, // makes env available everywhere
+          })
+        ],
   controllers: [AppController],
   providers: [AppService],
 })
